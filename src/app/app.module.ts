9e39bf125ec97a8bd3c33f8b083de048140740f7
NipuniManarangi <nipunimanarangi@gmail.com>
2020-07-06 20:39:44 +0530

    created the structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductComponent } from './components/product/product.component';
+import { CartItemComponent } from './components/cart/cart-item/cart-item.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    CartComponent,
+    ProductListComponent,
+    ProductComponent,
+    CartItemComponent,
+    OrderHistoryComponent
   ],
   imports: [
     BrowserModule,
